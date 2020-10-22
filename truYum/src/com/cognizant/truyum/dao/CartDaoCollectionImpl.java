cfd5f585f45b1a740e922894ecdf09a89c888f44
Manjunath159 <manjunath.u1998@gmail.com>
2020-10-22 18:15:43 +0530

    init setup

--- truYum/src/com/cognizant/truyum/dao/CartDaoCollectionImpl.java
+++ truYum/src/com/cognizant/truyum/dao/CartDaoCollectionImpl.java
@@ -1,5 +1,30 @@
 package com.cognizant.truyum.dao;
 
-public class CartDaoCollectionImpl {
+import java.util.HashMap;
+import java.util.List;
+
+import com.cognizant.truyum.model.Cart;
+
+public class CartDaoCollectionImpl implements CartDao {
+	private static HashMap<Long,Cart> userCarts;
+
+	@Override
+	public void addCartItem(long userId, long menuItemId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<MenuItem> getAllCartitems(long userId) throws CartEmptyException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeCartItem(long userId, long menuItemId) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
--- truYum/src/com/cognizant/truyum/dao/MenuItemDao.java
+++ truYum/src/com/cognizant/truyum/dao/MenuItemDao.java
@@ -1,13 +1,12 @@
 package com.cognizant.truyum.dao;
 
-import java.text.ParseException;
 import java.util.List;
 
 import com.cognizant.truyum.model.MenuItem;
 
 public interface MenuItemDao  {
 	   List<MenuItem> getMenuItemListAdmin();
-	   List<MenuItem> getMenuItemListCustomer() throws ParseException;
+	   List<MenuItem> getMenuItemListCustomer();
 	   void modifyMenuItem(MenuItem menuItem);
 	   MenuItem getMenuItem(long menuItemId);
 		 
