af2ee9608dccf53fcfc0683e62b59a4a10559f96
Manjunath159 <manjunath.u1998@gmail.com>
2020-10-24 12:53:14 +0530

    updated

--- truYum/src/com/cognizant/truyum/dao/MenuItemDaoSqlImpl.java
+++ truYum/src/com/cognizant/truyum/dao/MenuItemDaoSqlImpl.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import com.cognizant.truyum.model.Cart;
 import com.cognizant.truyum.model.MenuItem;
 
 public class MenuItemDaoSqlImpl implements MenuItemDao {
@@ -33,6 +34,7 @@ public class MenuItemDaoSqlImpl implements MenuItemDao {
 				lst.add(item);
 
 			}
+			ps.clearParameters();
 			return lst;
 
 		} catch (ClassNotFoundException e) {
@@ -60,6 +62,7 @@ public class MenuItemDaoSqlImpl implements MenuItemDao {
 						r.getDate(5), r.getString(6), r.getInt(7) == 1);
 				lst.add(item);
 			}
+			ps.clearParameters();
 			return lst;
 
 		} catch (ClassNotFoundException e) {
@@ -90,6 +93,7 @@ public class MenuItemDaoSqlImpl implements MenuItemDao {
 						r.getDate(5), r.getString(6), r.getInt(7) == 1);
 
 			}
+			ps.clearParameters();
 			return obj;
 
 		} catch (ClassNotFoundException e) {
@@ -117,6 +121,7 @@ public class MenuItemDaoSqlImpl implements MenuItemDao {
 			ps.setBoolean(6, menuItem.isFreeDelivery());
 			ps.setLong(7, menuItem.getId());
 			ps.executeUpdate();
+			ps.clearParameters();
 
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -125,5 +130,90 @@ public class MenuItemDaoSqlImpl implements MenuItemDao {
 		}
 
 	}
+	void addCartItem(long userId,long menuItemId) throws IOException {
+try {
+			
+			Connection connection = ConnectionHandler.getConnection();
+			
+			String query = "INSERT INTO CART(CT_USER_ID, CT_MENU_ID) VALUES (?, ?)";
+			ps = connection.prepareStatement(query);
+			ps.setLong(1, userId);
+			ps.setLong(2, menuItemId);
+			
+			
+			ps.clearParameters();
+			
+		} catch (ClassNotFoundException e) {
 
-}
+			e.printStackTrace();
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		
+	}
+	
+	List<MenuItem> getAllCartItems(long userId) throws CartEmptyException, IOException{
+		List<MenuItem> lst= new ArrayList<>();
+		Cart cart = new Cart(lst, 0);
+		double total = 0;
+		try {
+			Connection connection = ConnectionHandler.getConnection();
+			
+			String query = "SELECT * FROM MENU_ITEMS WHERE ID IN (SELECT CT_MENU_ID FROM CART WHERE CT_USER_ID = ?)";
+			ps = connection.prepareStatement(query);
+			
+			ps.setLong(1, userId);
+			
+			ResultSet r =ps.executeQuery();
+			
+			while(r.next()) {
+				
+				total += r.getFloat(3);
+				
+				MenuItem item = new MenuItem(r.getLong(1), r.getString(2), r.getFloat(3), r.getInt(4) == 1,
+						r.getDate(5), r.getString(6), r.getInt(7) == 1);
+				lst.add(item);
+			}
+			ps.clearParameters();
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		cart.setMenuItemList(lst);
+		cart.setTotal(total);
+		return lst;
+	}
+	void removeCartItem(long userId,long menuItemId) throws IOException {
+		try {
+			Connection connection = ConnectionHandler.getConnection();
+			String query = "DELETE FROM CART WHERE CT_USER_ID = ? AND CT_MENU_ID = ?";
+			
+			ps = connection.prepareStatement(query);
+			ps.setLong(1, userId);
+			ps.setLong(2,menuItemId);
+			
+			
+			ps.clearParameters();
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	}
+		
+
+	
+	
+
+
--- truYum/src/com/cognizant/truyum/model/Cart.java
+++ truYum/src/com/cognizant/truyum/model/Cart.java
@@ -4,22 +4,37 @@ import java.util.List;
 
 public class Cart {
 
+	private List<MenuItem> newUserMenuList;
+	private double total;
+
 	public Cart(List<MenuItem> newUserMenuList) {
+		super();
+		this.newUserMenuList=newUserMenuList;
+		
 		// TODO Auto-generated constructor stub
+	}
+
+	public Cart(List<MenuItem> newUserMenuList, double total) {
+		// TODO Auto-generated constructor stub
+		super();
+		this.newUserMenuList=newUserMenuList;
+		this.total=total;
 	}
 
 	public List<MenuItem> getMenuItemList() {
 		// TODO Auto-generated method stub
-		return null;
+		return  newUserMenuList;
 	}
 
-	public void setMenuItemList(List<MenuItem> menuItemList) {
+	public void setMenuItemList(List<MenuItem> newUserMenuList) {
 		// TODO Auto-generated method stub
+		this.newUserMenuList=newUserMenuList;
 		
 	}
 
 	public void setTotal(double total) {
 		// TODO Auto-generated method stub
+		this.total=total;
 		
 	}
 
